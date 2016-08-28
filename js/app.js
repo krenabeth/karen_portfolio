8eabbcd1fe582701ba231b543a8ee2f5da3c22c9
Karen Langner <krenabeth@yahoo.com>
2016-08-27 22:09:58 -0400

    cleaning up

--- js/app.js
+++ js/app.js
@@ -4,16 +4,5 @@ app.directive('project', function() {
 	return {
 		restrict: 'E',
 		templateUrl: 'templates/project.html'
-		// add link here
 	};
 });
-
-// app.directive('descrip',function($scope, projectCtrl){
-//   return {
-//       restrict: 'E',
-//       replace: true, 
-//       scope: true,
-//       template: "ng-click=(alert({{ projects.description }})"
-//   }     
-// });
-  
--- js/scripts.js
+++ js/scripts.js
@@ -12,69 +12,20 @@ document.getElementById("nav01").innerHTML =
 // here's some jQuery
 $(document).ready(function() {
 
-
-
-$('#button').on('click', function(event) {
-	console.log("hi");
-  $( '#contact' ).hide(); //this line not doing anything
-   	console.log("bye");
-
-});
-
-
 // global variable for li
-var $tab = $('.tab');
+	var $tab = $('.tab');
 // tabs are hidden, then slide down
-$(function() {
-	$($tab).hide().slideDown('slow');
-});
-// styling for tab of current page
-// find div -ids:  main about folio codes contact - (could locate as fourth child of the body element) 
-// $(document).load(function() {
-// // remove the current class on all tabs
-// // if the fourth child of the body is main
-// 	if (div.find($'.main')) {
-// 		// find li with id home and make the class current instead of tab
-// 	}
-// // if the fourth child is about
-// 	else if (div.find($'about')){
-// 		// find the li with id about and make the class current instead of tab
-// 	}
-// // if the fourth child is folio
-// 	else if (div.find($'folio'){
-
-// 	}
-// 	// codes
-// 	else if {
-// 		//contact
-// 	}
-// 	else {
-// 		console.log('contact info');
-// 	}
-// });
-// declare the above as variable $current
-// var $current = ;
-// $($current).removeAttr('tab'); // remove the attribute of tab class
-// $($current).attr('class', 'current'); // add the current class attribute
-
-// flip the sections on index.html with div id's main codes contact according to which corresponding tab was clicked on
-// for each tab, fade to 0.25 opacity for hover
-$tab.hover(function() {
-	$( this ).fadeTo('slow', 0.25);	
-	},
-//return to full opacity when cursor removed
-	function(){
-	$( this ).fadeTo('slow', 1);
+	$(function() {
+		$($tab).hide().slideDown('slow');
 	});
-});
-// swith the photos on about page
-// $('#aboutImg2').hide();
 
-// $( "#aboutImg1" ).hide(function() {
-//   $( "this" ).toggle().fadeIn( "slow", function() {
-//     $( "#aboutImg1" ).fadeOut("slow", 100);
-//   });
+// for each tab, fade to 0.25 opacity for hover
+	$tab.hover(function() {
+		$( this ).fadeTo('slow', 0.25);	
+		},
+//return to full opacity when cursor removed
+		function(){
+			$( this ).fadeTo('slow', 1);
+		});
 
-
-
- // closing out document ready
+}); // closing out document ready
