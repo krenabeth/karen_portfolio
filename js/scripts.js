c426e7f3387888620432aae06c2838d9fa6e0477
Karen Langner <krenabeth@yahoo.com>
2016-09-26 21:51:50 -0400

    navigation styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 //navigation bar
 document.getElementById("nav01").innerHTML =
 	"<ul id='navBar'>" +
-		"<li id='home' class='tab'><a href='index.html'>Home</a></li>" +
-		"<li id='about' class='tab'><a href='about.html'>About</a></li>" +
-		"<li id='portfolio' class='tab'><a href='portfolio.html'>Portfolio</a></li>" 
+		"<li><div id='home' class='tab'><a href='index.html'>Home</a></div></li>" +
+		"<li><div id='about' class='tab'><a href='about.html'>About</a></div></li>" +
+		"<li><div id='portfolio' class='tab'><a href='portfolio.html'>Portfolio</a></div></li>" 
 	"</ul>";
 
 // here's some jQuery
@@ -15,14 +15,5 @@ $(document).ready(function() {
 	$(function() {
 		$($tab).hide().slideDown('slow');
 	});
-
-// for each tab, fade to 0.25 opacity for hover
-	$tab.hover(function() {
-		$( this ).fadeTo('slow', 0.25);	
-		},
-//return to full opacity when cursor removed
-		function(){
-			$( this ).fadeTo('slow', 1);
-		});
 
 }); // closing out document ready
