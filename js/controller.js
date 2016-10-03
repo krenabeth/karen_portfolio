1433b323765b435d5746310bc321a96872394150
Karen Langner <krenabeth@yahoo.com>
2016-10-02 23:16:19 -0400

    project descriptions

--- js/controller.js
+++ js/controller.js
@@ -7,51 +7,51 @@ app.controller('projectCtrl', function($scope) {
 			title: "Lockbox",
 			image: {
 				path: "images/lockbox_screenshot.png",
-				altText: "lockbox"
+				altText: "lockbox screenshot"
 			},
 			link: "https://github.com/krenabeth/lockbox",
 			skills: ['JavaScript', 'AngularJS'],
-			description: "Game app for 2nd grade level, practicing math and life skills"
+			description: "Game app for 2nd grade level, practicing math and life skills, using JavaScript and AngularJS"
 
 		},
 		{
 			title: "cat match",
 			image: {
 				path: "images/catmatch_screenshot.png",
-				altText: "catmatch"
+				altText: "catmatch screenshot"
 			},
 			link: "https://github.com/krenabeth/catmatch",
-			skills: ['AngularJS', 'jQuery', 'API'],
-			description: "Spoof of match.com with petfinder.com API"
+			skills: ['AngularJS', 'jQuery'],
+			description: "Spoof of match.com with petfinder.com API, AngularJS, and jQuery"
 
 		},
 		{
 			title: "FitKnit",
 			image: {
 				path: "images/fitknit.png",
-				altText: "FitKnit"
+				altText: "FitKnit screenshot"
 			},
 			link: "http://fitknit.herokuapp.com",
 			skills: ['AngularJS'],
-			description: "Group project - final project for Grand Circus bootcamp. Fitbit user application"
+			description: "Final group project for Grand Circus bootcamp - Fitbit user application using AngularJS"
 
 		},
 		{
 			title: "Art by Pat Langner",
 			image: {
 				path: "images/plangner.png",
-				altText: ""
+				altText: "Art by Pat Langner screenshot"
 			},
 			link: "http://www.patlangner.com",
 			skills: ['JavaScript', 'AngularJS'],
-			description: "artist gallery"
+			description: "Artist Pat Langner - artist information and gallery using Lightbox plugin, AngularJS, JavaScript"
 
 		},
 		{
 			title: "Talk-a-thon",
 			image: {
 				path: "images/talk_a_thon_project.png",
-				altText: ""
+				altText: "Talk-a-thon screenshot"
 			},
 			link: "https://github.com/krenabeth/lab13-talk-a-thon",
 			skills: ['JavaScript', 'AngularJS'],
@@ -73,18 +73,18 @@ app.controller('projectCtrl', function($scope) {
 			title: "Travel the World",
 			image: {
 				path: "images/travel.png",
-				altText: ""
+				altText: "Travel site screenshot"
 			},
 			link: "https://github.com/joshtlk1/TravelTheWord_flickr_apilab18",
 			skills: ['AngularJS'],
-			description: "Group project, lab 18 for Grand Circus"
+			description: "Group project, lab 18 for Grand Circus using AngularJS"
 
 		}, 
 		{
 			title: "Poetry Blog",
 			image: {
 				path: "images/poetry.png",
-				altText: ""
+				altText: "blog screenshot"
 			},
 			link: "https://github.com/krenabeth/poetry_blog",
 			skills: ['Bootstrap'],
@@ -95,7 +95,7 @@ app.controller('projectCtrl', function($scope) {
 			title: "Choosing a Pet",
 			image: {
 				path: "images/lab14-cat-dog.png",
-				altText: ""
+				altText: "Pet site screenshot"
 			},
 			link: "https://github.com/krenabeth/lab14-cat-dog",
 			skills: ['JavaScript', 'AngularJS'],
